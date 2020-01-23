7a3c7eee5521165cbab31fd8ef0e66bc8272a8f2
Jibi Jose <jibijose@yahoo.com>
2020-01-23 05:49:12 +0530

    run all remote gatling

---
+++ runAllRemoteReportCluster.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#./setupVMs.sh $1 $2
+./setupGatlings.sh $1 $2
+
+./startGatlings.sh $1 $2
+./waitGatlings.sh $1 $2
+#./stopGatlings.sh $1 $2
+./reportWithRemoteGatling.sh $1 $2
+
+echo "Remote execution completed"
